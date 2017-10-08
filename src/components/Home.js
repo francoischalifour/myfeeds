9d319b39eb4753044dd01379a4c090908f25a9d9
francoischalifour <francois.chalifour@gmail.com>
2017-10-08 12:08:22 -0400

    Update Home to functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { getCurrentUserId } from '../utils'
 import { getUserById } from '../api/users'
 import Scaffold from './Scaffold'
@@ -7,21 +7,17 @@ import ProfileSidebar from './ProfileSidebar'
 import Feed from './Feed'
 import PostForm from './PostForm'
 
-export default class Home extends Component {
-  state = {
-    userId: getCurrentUserId(),
-  }
+const Home = () => {
+  const user = getUserById(getCurrentUserId())
 
-  render() {
-    const user = getUserById(this.state.userId)
-
-    return (
-      <Scaffold grid>
-        <ProfileSidebar {...user} />
-        <Main>
-          <Feed renderHeader={() => <PostForm {...user} />} />
-        </Main>
-      </Scaffold>
-    )
-  }
+  return (
+    <Scaffold grid>
+      <ProfileSidebar {...user} />
+      <Main>
+        <Feed renderHeader={() => <PostForm {...user} />} />
+      </Main>
+    </Scaffold>
+  )
 }
+
+export default Home
