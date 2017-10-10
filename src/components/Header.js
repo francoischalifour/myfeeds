1766b71015e609ee463a19812e94d7d4079a7469
francoischalifour <francois.chalifour@gmail.com>
2017-10-09 22:23:57 -0400

    Limit navbar width

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,21 @@ import MdSearch from 'react-icons/lib/md/search'
 import SearchForm from './SearchForm'
 
 const Navbar = glamorous.header({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  height: 56,
   backgroundColor: '#212121',
-  padding: '16px 24px',
   color: '#fff',
   boxShadow: '0 1px 3px rgba(50,50,50,.16)',
   '& a': {
     color: '#4fc3f7',
   },
+})
+
+const Container = glamorous.div({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  margin: '0 auto',
+  padding: '12px 24px',
+  maxWidth: 1200,
 })
 
 const Title = glamorous.h1({
@@ -36,22 +40,24 @@ const Header = () => {
 
   return (
     <Navbar>
-      <Title>
-        <Link to="/">
-          <MdLineWeight size="24" color="#fff" /> myfeeds
-        </Link>
-      </Title>
+      <Container>
+        <Title>
+          <Link to="/">
+            <MdLineWeight size="24" color="#fff" /> myfeeds
+          </Link>
+        </Title>
 
-      <Searchbar>
-        <MdSearch size="24" color="#eee" style={{ marginTop: 6 }} />
-        <SearchForm value={search} />
-      </Searchbar>
+        <Searchbar>
+          <MdSearch size="24" color="#eee" style={{ marginTop: 6 }} />
+          <SearchForm value={search} />
+        </Searchbar>
 
-      <ul>
-        <li>
-          <Link to="/logout">Logout</Link>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        </ul>
+      </Container>
     </Navbar>
   )
 }
