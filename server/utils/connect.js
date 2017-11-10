f68969f6c72ee66e10d8ef6d5e0fa1fbd22f39ad
francoischalifour <francois.chalifour@gmail.com>
2017-11-09 19:35:06 -0500

    Refactor connect()

--- server/utils/connect.js
+++ server/utils/connect.js
@@ -1,16 +1,19 @@
-/**
- * This script connects to the MongoDB database with the given environment variables
- * and returns the database instance.
- */
 require('dotenv').config()
 const { MongoClient } = require('mongodb')
 
 const { DB_HOST, DB_PORT, DB_NAME } = process.env
 
-if (!DB_HOST || !DB_PORT || !DB_NAME) {
-  throw new Error('⚠️ Missing database connection information.')
+/**
+ * Returns an instance of the MongoDB database.
+ *
+ * By default, this instance is defined with environment variables.
+ */
+const connect = (host = DB_HOST, port = DB_PORT, name = DB_NAME) => {
+  if (!host || !port || !name) {
+    throw new Error('⚠️ Missing database connection information.')
+  }
+
+  return MongoClient.connect(`mongodb://${host}:${port}/${name}`)
 }
 
-const DB_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
-
-module.exports = () => MongoClient.connect(DB_URL)
+module.exports = connect
