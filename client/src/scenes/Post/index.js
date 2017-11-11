4b23c802fbad78479a496361d0a9f4a039f2aa16
francoischalifour <francois.chalifour@gmail.com>
2017-11-10 19:56:18 -0500

    Refactor scenes/Post

--- client/src/scenes/Post/index.js
+++ client/src/scenes/Post/index.js
@@ -20,28 +20,37 @@ const Container = glamorous.div({
 
 class PostScene extends Component {
   state = {
-    post: {},
+    loading: true,
+    error: false,
     hasReplied: false,
     hasFavorited: false,
-    replies: [],
   }
 
   async componentDidMount() {
     this.activeUser = await api.getUserById(getCurrentUserId())
     const postId = this.props.match.params.postid
     const post = await api.getPostById(postId)
-    const replies = await api.getPostRepliesById(postId)
-    const userMetadata = await api.getUserPostData({
-      post_id: postId,
-      user_id: this.activeUser._id,
-    })
 
-    this.setState({
-      post,
-      replies,
-      hasReplied: userMetadata.replied,
-      hasFavorited: userMetadata.favorited,
-    })
+    if (post._id) {
+      const replies = await api.getPostRepliesById(postId)
+      const userMetadata = await api.getUserPostData({
+        post_id: postId,
+        user_id: this.activeUser._id,
+      })
+
+      this.setState({
+        loading: false,
+        post,
+        replies,
+        hasReplied: userMetadata.replied,
+        hasFavorited: userMetadata.favorited,
+      })
+    } else {
+      this.setState({
+        loading: false,
+        error: true,
+      })
+    }
   }
 
   onFavorite = async postId => {
@@ -71,6 +80,7 @@ class PostScene extends Component {
       parent_id: postId,
     }
     const success = !!await api.addPost(post)
+
     if (success) {
       const replies = await api.getPostRepliesById(postId)
       const post = await api.getPostById(postId)
@@ -83,38 +93,56 @@ class PostScene extends Component {
     }
   }
 
+  renderLoading = () => {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
+  renderNotFound = () => {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <MdFindInPage size={212} color="#ddd" />
+        <p>This post doesn't exist.</p>
+      </div>
+    )
+  }
+
+  renderPost = () => {
+    return (
+      <Container>
+        <Post
+          {...this.state.post}
+          hasReplied={this.state.hasReplied}
+          hasFavorited={this.state.hasFavorited}
+          onFavorite={this.onFavorite}
+        />
+
+        <Feed
+          posts={this.state.replies}
+          renderHeader={() => (
+            <PostForm
+              placeholder={`Reply to @${this.state.post.username || ''}`}
+              parentId={this.state.post._id}
+              onSubmit={this.onSubmit}
+              {...this.activeUser}
+            />
+          )}
+        />
+      </Container>
+    )
+  }
+
   render() {
     return (
       <Scaffold grid>
         <ProfileSidebar {...this.activeUser} />
         <Content>
-          {!this.state.post ? (
-            <div style={{ textAlign: 'center' }}>
-              <MdFindInPage size={212} color="#ddd" />
-              <p>This post doesn't exist.</p>
-            </div>
-          ) : (
-            <Container>
-              <Post
-                {...this.state.post}
-                hasReplied={this.state.hasReplied}
-                hasFavorited={this.state.hasFavorited}
-                onFavorite={this.onFavorite}
-              />
-
-              <Feed
-                posts={this.state.replies}
-                renderHeader={() => (
-                  <PostForm
-                    placeholder={`Reply to @${this.state.post.username || ''}`}
-                    parentId={this.state.post._id}
-                    onSubmit={this.onSubmit}
-                    {...this.activeUser}
-                  />
-                )}
-              />
-            </Container>
-          )}
+          {this.state.loading
+            ? this.renderLoading()
+            : this.state.error ? this.renderNotFound() : this.renderPost()}
         </Content>
       </Scaffold>
     )
