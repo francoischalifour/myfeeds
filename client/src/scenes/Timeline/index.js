22a32dedc42e5f9c074f31809e74dc7698cbc229
francoischalifour <francois.chalifour@gmail.com>
2017-11-10 20:09:52 -0500

    Refactor scenes/Timeline

--- client/src/scenes/Timeline/index.js
+++ client/src/scenes/Timeline/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import MdCloudOff from 'react-icons/lib/md/cloud-off'
 import { getCurrentUserId } from 'utils'
 import api from 'api'
 import Scaffold from 'components/Scaffold'
@@ -9,16 +10,69 @@ import PostForm from 'components/PostForm'
 
 class TimelineScene extends Component {
   state = {
-    posts: [],
+    loading: true,
+    error: false,
   }
 
   async componentDidMount() {
     this.activeUser = await api.getUserById(getCurrentUserId())
     const posts = await api.getAllPosts()
 
-    this.setState({
-      posts,
-    })
+    if (posts.length > 0) {
+      this.setState({
+        loading: false,
+        posts,
+      })
+    } else {
+      this.setState({
+        loading: false,
+        error: true,
+      })
+    }
+  }
+
+  onSubmit = async text => {
+    const post = {
+      text,
+      user_id: this.activeUser._id,
+    }
+    const success = !!await api.addPost(post)
+
+    if (success) {
+      const posts = await api.getAllPosts()
+
+      this.setState({
+        posts,
+      })
+    }
+  }
+
+  renderLoading = () => {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
+  renderError = () => {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <MdCloudOff size={200} color="#bbb" />
+        <p>We can't retrieve the timeline for now 😕</p>
+      </div>
+    )
+  }
+
+  renderTimeline = () => {
+    return (
+      <Feed
+        posts={this.state.posts}
+        renderHeader={() => (
+          <PostForm {...this.activeUser} onSubmit={this.onSubmit} />
+        )}
+      />
+    )
   }
 
   render() {
@@ -26,10 +80,9 @@ class TimelineScene extends Component {
       <Scaffold grid>
         <ProfileSidebar {...this.activeUser} />
         <Content>
-          <Feed
-            posts={this.state.posts}
-            renderHeader={() => <PostForm {...this.activeUser} />}
-          />
+          {this.state.loading
+            ? this.renderLoading()
+            : this.state.error ? this.renderError() : this.renderTimeline()}
         </Content>
       </Scaffold>
     )
