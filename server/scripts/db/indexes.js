0a9bcca62e732f8b26bf1b49febc1a5693bedbf6
francoischalifour <francois.chalifour@gmail.com>
2017-11-16 14:54:45 -0500

    Create indexes schema

---
+++ server/scripts/db/indexes.js
@@ -0,0 +1,48 @@
+const {
+  COLLECTION_USERS,
+  COLLECTION_POSTS,
+  COLLECTION_FAVORITES,
+} = require('../../constants')
+
+const indexes = {
+  [COLLECTION_USERS]: [
+    {
+      description: "get a user's profile",
+      value: [{ username: 1 }, { unique: true }],
+    },
+    {
+      description: "get a user's account (login and signup)",
+      value: [{ email: 1 }, { unique: true }],
+    },
+  ],
+  [COLLECTION_POSTS]: [
+    {
+      description: 'get ordered feeds',
+      value: [{ created_at: 1 }],
+    },
+    {
+      description: 'search for posts',
+      value: [{ text: 'text' }],
+    },
+    {
+      description: "get a post's user metadata",
+      value: [{ user_id: 1, parent_id: 1 }],
+    },
+  ],
+  [COLLECTION_FAVORITES]: [
+    {
+      description: "get a post's favs",
+      value: [{ post_id: 1 }],
+    },
+    {
+      description: 'get ordered favs',
+      value: [{ created_at: 1 }],
+    },
+    {
+      description: 'check if a user has favorited',
+      value: [{ post_id: 1, user_id: 1 }],
+    },
+  ],
+}
+
+module.exports = indexes
--- server/scripts/db/init.js
+++ server/scripts/db/init.js
@@ -4,8 +4,8 @@
  *  - Creates the necessary collections
  *  - Creates indexes
  */
-const { MongoClient } = require('mongodb')
 const { connect } = require('../../utils/db')
+const indexes = require('./indexes')
 const {
   COLLECTION_USERS,
   COLLECTION_POSTS,
@@ -18,67 +18,38 @@ const DB_COLLECTIONS = [
   COLLECTION_FAVORITES,
 ]
 
-const createIndexes = async db => {
-  /**
-   * Users
-   */
-  await db
-    .collection(COLLECTION_USERS)
-    .createIndex({ username: 1 }, { unique: true })
-    .catch(err => console.error(err))
-  await db
-    .collection(COLLECTION_USERS)
-    .createIndex({ email: 1 }, { unique: true })
-    .catch(err => console.error(err))
-
-  /**
-   * Posts
-   */
-  await db
-    .collection(COLLECTION_POSTS)
-    .createIndex({ text: 'text' })
-    .catch(err => console.error(err))
-  // To retrieve post's user metadata
-  await db
-    .collection(COLLECTION_POSTS)
-    .createIndex({ user_id: 1, parent_id: 1 })
-    .catch(err => console.error(err))
-
-  /**
-   * Stars
-   */
-  await db
-    .collection(COLLECTION_FAVORITES)
-    .createIndex({ post_id: 1 })
-    .catch(err => console.error(err))
-  await db
-    .collection(COLLECTION_FAVORITES)
-    .createIndex({ post_id: 1, user_id: 1 })
-    .catch(err => console.error(err))
-}
-
 const run = async () => {
   const db = await connect()
 
-  const collections = (await db.collections().catch(err => console.error(err)))
+  const collections = (await db.collections().catch(console.error))
     .map(({ s: { name } }) => name)
     .filter(name => name !== 'system.indexes')
 
+  console.log('🔥 Dropping collections')
   for (let name of collections) {
-    console.log(`🔥 Dropping collection "${name}"...`)
-    await db.dropCollection(name).catch(err => console.error(err))
+    console.log(`  ▶ ${name}`)
+    await db.dropCollection(name).catch(console.error)
   }
 
+  console.log('\n📦 Creating collections')
   for (let name of DB_COLLECTIONS) {
-    console.log(`📦 Creating collection "${name}"...`)
-    await db.createCollection(name).catch(err => console.error(err))
+    console.log(`  ▶ ${name}`)
+    await db.createCollection(name).catch(console.error)
   }
 
-  console.log(`⚡️ Settings indexes...`)
-  await createIndexes(db)
+  console.log(`\n⚡️ Setting indexes`)
+  for (let collection in indexes) {
+    for (let index of indexes[collection]) {
+      console.log(`  ▶ ${collection}: ${index.description}`)
+      await db
+        .collection(collection)
+        .createIndex(...index.value)
+        .catch(console.error)
+    }
+  }
 
-  await db.close().catch(err => console.error(err))
-  console.log('\n👍 All done initializing the database!')
+  await db.close().catch(console.error)
+  console.log('\n👍 All done initializing the database!\n')
 }
 
 run()
